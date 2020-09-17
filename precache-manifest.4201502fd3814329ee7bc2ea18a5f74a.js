f561082348e4573a2739b4330056727b26ee715c
Senem Soy <ssoy23@gmail.com>
2020-09-17 16:23:53 -0700

    Updates

---
+++ precache-manifest.4201502fd3814329ee7bc2ea18a5f74a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d997937a5c66131f0ea3cf9ca2dd77d0",
+    "url": "/senemsoy/chatty/index.html"
+  },
+  {
+    "revision": "298a13bd34566b9d50b7",
+    "url": "/senemsoy/chatty/static/css/main.1e4c3c58.chunk.css"
+  },
+  {
+    "revision": "29dcf5c5895db4ebdcda",
+    "url": "/senemsoy/chatty/static/js/2.7a528f03.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/senemsoy/chatty/static/js/2.7a528f03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "298a13bd34566b9d50b7",
+    "url": "/senemsoy/chatty/static/js/main.a960ba32.chunk.js"
+  },
+  {
+    "revision": "c5adb003170484684b6e",
+    "url": "/senemsoy/chatty/static/js/runtime-main.2e9cc56e.js"
+  }
+]);
